5f892d14ea1f82c3b1b23eef500f87724a0ebf96
kimszin <kimszin@naver.com>
2023-06-28 09:06:42 +0900

    console 삭제

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 const NotFound = (props) => {
-    console.log(props)
-    return (
+  return (
     <div>
-        <h1>주소가 올바르지 않아요!</h1>
-        <button onClick={()=>{props.history.goBack();}}>뒤로가기</button>
+      <h1>주소가 올바르지 않아요!</h1>
+      <button
+        onClick={() => {
+          props.history.goBack();
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
-    );
-}
+  );
+};
 
 export default NotFound;
--- src/redux/modules/bucket.js
+++ src/redux/modules/bucket.js
@@ -1,5 +1,5 @@
 // bucket.js
-import {firestore} from "../../firebase";
+import { firestore } from "../../firebase";
 
 const bucket_db = firestore.collection("bucket");
 
@@ -14,176 +14,175 @@ const LOADED = "bucket/LOADED";
 
 // InitialState
 const InitialState = {
-    list: [
-        { text: "영화관 가기", completed: false },
-        { text: "매일 책읽기", completed: false },
-        { text: "수영 배우기", completed: false },
-    ],
-    is_loaded: false,
-}
+  list: [
+    { text: "영화관 가기", completed: false },
+    { text: "매일 책읽기", completed: false },
+    { text: "수영 배우기", completed: false },
+  ],
+  is_loaded: false,
+};
 
 // Action Creators
 export const loadBucket = (bucket) => {
-    return {type: LOAD, bucket};
-}
+  return { type: LOAD, bucket };
+};
 
 export const createBucket = (bucket) => {
-    return {type: CREATE, bucket};
-}
+  return { type: CREATE, bucket };
+};
 
 export const updateBucket = (bucket, isComplete) => {
-    if(isComplete === "false"){
-        return {type: UPDATE, bucket};
-    }else{
-        return {type: UPDATE_CANCEL, bucket};
-    }
-}
+  if (isComplete === "false") {
+    return { type: UPDATE, bucket };
+  } else {
+    return { type: UPDATE_CANCEL, bucket };
+  }
+};
 
 export const isLoaded = (loaded) => {
-    return {type: LOADED, loaded};
-}
+  return { type: LOADED, loaded };
+};
 
 export const deleteBucket = (bucket) => {
-    return {type: DELETE, bucket};
-}
+  return { type: DELETE, bucket };
+};
 
 //
 export const loadBucketFB = () => {
-    return function (dispatch) {
+  return function (dispatch) {
+    bucket_db.get().then((docs) => {
+      let bucket_data = [];
 
-        bucket_db.get().then((docs) => {
-            let bucket_data = [];
-            
-            docs.forEach((doc) => {
-                if(doc.exists) {
-                    bucket_data = [...bucket_data, {id: doc.id, ...doc.data()}];
-                }
-            })
+      docs.forEach((doc) => {
+        if (doc.exists) {
+          bucket_data = [...bucket_data, { id: doc.id, ...doc.data() }];
+        }
+      });
 
-            dispatch(loadBucket(bucket_data));
-        });
-    };
-}
+      dispatch(loadBucket(bucket_data));
+    });
+  };
+};
 
 export const addBucketFB = (bucket) => {
-    return function (dispatch) {
-        let bucket_data = {text:bucket, completed: false};
-        console.log('여기',bucket_data)
+  return function (dispatch) {
+    let bucket_data = { text: bucket, completed: false };
 
-        dispatch(isLoaded(false));
+    dispatch(isLoaded(false));
 
-        bucket_db.add(bucket_data).then(docRef => {
-            bucket_data = {...bucket_data, id: docRef.id};
-            console.log('zz',bucket_data)
-            dispatch(createBucket(bucket_data));
-            dispatch(isLoaded(true));
-        })
+    bucket_db.add(bucket_data).then((docRef) => {
+      bucket_data = { ...bucket_data, id: docRef.id };
+      dispatch(createBucket(bucket_data));
+      dispatch(isLoaded(true));
+    });
+  };
+};
+
+export const updateBucketFB = (bucket, isComplete) => {
+  return function (dispatch, getState) {
+    const old_bucket_data = getState().bucket.list[bucket];
+    let bucket_data;
+    if (isComplete === "false") {
+      bucket_data = { ...old_bucket_data, completed: true };
+    } else {
+      bucket_data = { ...old_bucket_data, completed: false };
     }
-}
+    // ↑ 전체 내용이 아닌 completed만 넘겨줘도 된다.
 
-export const updateBucketFB = (bucket, isComplete) => { 
-    return function (dispatch, getState) {
-        const old_bucket_data = getState().bucket.list[bucket];
-        console.log(old_bucket_data);
-        let bucket_data; 
-        if(isComplete === "false"){
-            bucket_data = {...old_bucket_data, completed: true}; 
-        }else{
-            bucket_data = {...old_bucket_data, completed: false}; 
-        }
-        // ↑ 전체 내용이 아닌 completed만 넘겨줘도 된다.
-
-        if(!bucket_data.id){
-            return;
-        }
-        dispatch(isLoaded(false));
-
-        bucket_db.doc(bucket_data.id).update(bucket_data).then(docRef => { 
-            dispatch(updateBucket(bucket, isComplete));
-            dispatch(isLoaded(true));
-        }).catch(error => {
-            console.log(error);
-        });
+    if (!bucket_data.id) {
+      return;
     }
-}
+    dispatch(isLoaded(false));
 
-export const deleteBucketFB = (bucket) => { 
-    return function (dispatch, getState) {
-        const old_bucket_data = getState().bucket.list[bucket];
+    bucket_db
+      .doc(bucket_data.id)
+      .update(bucket_data)
+      .then((docRef) => {
+        dispatch(updateBucket(bucket, isComplete));
+        dispatch(isLoaded(true));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
 
-        if(!old_bucket_data.id){
-            return;
-        }
-        dispatch(isLoaded(false));
+export const deleteBucketFB = (bucket) => {
+  return function (dispatch, getState) {
+    const old_bucket_data = getState().bucket.list[bucket];
 
-        bucket_db.doc(old_bucket_data.id).delete().then(docRef => {
-            dispatch(deleteBucket(bucket));
-            dispatch(isLoaded(true));
-        }).catch(error => { 
-            // delete 요청이 실패했으면(삭제가 안됬으면) then이 아니라 catch로 빠짐
-            // 혹은 then 안에서 고의적으로 에러를 내도 catch로 빠진다.
-            console.log(error) 
-        })
+    if (!old_bucket_data.id) {
+      return;
     }
-}
+    dispatch(isLoaded(false));
+
+    bucket_db
+      .doc(old_bucket_data.id)
+      .delete()
+      .then((docRef) => {
+        dispatch(deleteBucket(bucket));
+        dispatch(isLoaded(true));
+      })
+      .catch((error) => {
+        // delete 요청이 실패했으면(삭제가 안됬으면) then이 아니라 catch로 빠짐
+        // 혹은 then 안에서 고의적으로 에러를 내도 catch로 빠진다.
+        console.log(error);
+      });
+  };
+};
 
 // Reducer
 export default function reducer(state = InitialState, action = {}) {
   switch (action.type) {
     // do reducer stuff
     case "bucket/LOAD": {
-        if(action.bucket.length > 0){
-            console.log(action.bucket)
-            return {list: action.bucket, is_loaded : true};
-        }
-        console.log(state)
-        return state;
+      if (action.bucket.length > 0) {
+        return { list: action.bucket, is_loaded: true };
+      }
+      return state;
     }
 
     case "bucket/CREATE": {
-        const new_bucket_list = [...state.list, action.bucket];
-        console.log(new_bucket_list)
-        return {list: new_bucket_list};
+      const new_bucket_list = [...state.list, action.bucket];
+      return { list: new_bucket_list };
     }
-    
+
     case "bucket/UPDATE": {
-        const bucket_list = state.list.map((l, idx) => {
-            if(idx === action.bucket) {
-                return {...l, completed: true};
-            } else{
-                return l;
-            }
-        });
-        return {list: bucket_list};
+      const bucket_list = state.list.map((l, idx) => {
+        if (idx === action.bucket) {
+          return { ...l, completed: true };
+        } else {
+          return l;
+        }
+      });
+      return { list: bucket_list };
     }
-    
+
     case "bucket/UPDATE_CANCEL": {
-        const bucket_list = state.list.map((l, idx) => {
-            if(idx === action.bucket) {
-                return {...l, completed: false};
-            } else{
-                return l;
-            }
-        });
-        return {list: bucket_list};
+      const bucket_list = state.list.map((l, idx) => {
+        if (idx === action.bucket) {
+          return { ...l, completed: false };
+        } else {
+          return l;
+        }
+      });
+      return { list: bucket_list };
     }
 
     case "bucket/DELETE": {
-        const bucket_list = state.list.filter((l, idx) => {
-            if(idx !== action.bucket){
-                return l;
-            }
-          });
-          return {list: bucket_list};
-    }
-        
-    case "bucket/LOADED": {
-        return {...state, is_loaded: action.loaded};
+      const bucket_list = state.list.filter((l, idx) => {
+        if (idx !== action.bucket) {
+          return l;
+        }
+      });
+      return { list: bucket_list };
     }
 
-    default: return state;
+    case "bucket/LOADED": {
+      return { ...state, is_loaded: action.loaded };
+    }
+
+    default:
+      return state;
   }
 }
-
-
-
